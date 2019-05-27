7f0290d71edf775035ba25f504a4f41f8cf96b41
Jorge <tetris.attack@hotmail.com>
2019-05-27 04:06:12 -0300

    Lucas originals (Segmentação.zip) edit dirs
    para compatibilidade com a estrutura definida

--- YOLO/cut_nasal_pattern.py
+++ YOLO/cut_nasal_pattern.py
@@ -1,35 +1,43 @@
 import cv2
+import os
+import fnmatch
 
-arquivo = open("usp_focinhos.csv", "r")
+arquivo = open("iapar2_focinhos.csv", "r")
 
 for linha in arquivo:
-	linha = linha[:-2]
+    linha = linha[:-2]
 
-	linha = linha.replace("\n", "")
-	
-	linha = linha.split(",")
+    linha = linha.replace("\n", "")
+    
+    linha = linha.split(",")
+    
+    # procura a linha de nome de arquivo correspondente
+    for root, dirnames, filenames in os.walk('../imgs/Projeto_IAPAR/Base_Jersey/~rng'):
+        for filename in fnmatch.filter(filenames, linha[0]):
+            imgpath = os.path.join(root, filename)
+            break
 
-	img = cv2.imread("USP/USP/" + linha[0])
+    img = cv2.imread(imgpath)
 
-	if len(linha) == 9:
-		x1 = int(linha[1])
-		y1 = int(linha[2])
-		w1 = int(linha[3])
-		h1 = int(linha[4])
+    if len(linha) == 9:
+        x1 = int(linha[1])
+        y1 = int(linha[2])
+        w1 = int(linha[3])
+        h1 = int(linha[4])
 
-		x2 = int(linha[5])
-		y2 = int(linha[6])
-		w2 = int(linha[7])
-		h2 = int(linha[8])
+        x2 = int(linha[5])
+        y2 = int(linha[6])
+        w2 = int(linha[7])
+        h2 = int(linha[8])
 
-		x_esq = min(x1 + w1, x2 + w2)
-		x_dir = max(x1, x2)
+        x_esq = min(x1 + w1, x2 + w2)
+        x_dir = max(x1, x2)
 
-		dim = x_dir - x_esq
+        dim = x_dir - x_esq
 
-		y_top = int((y1 + 0.4 * h1 + y2 + 0.4 * h2) / 2)
-		y_bot = y_top + dim
+        y_top = int((y1 + 0.4 * h1 + y2 + 0.4 * h2) / 2)
+        y_bot = y_top + dim
 
-		if y_top < y_bot and x_esq < x_dir:
-			roi = img[y_top : y_bot, x_esq : x_dir]
-			cv2.imwrite("USP_nasal_pattern/" + linha[0], roi)
+        if y_top < y_bot and x_esq < x_dir:
+            roi = img[y_top : y_bot, x_esq : x_dir]
+            cv2.imwrite("IAPAR2_nasal_pattern/" + linha[0], roi)
