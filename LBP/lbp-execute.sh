c1c3c82c468ec34cb9bc2e0ad37b6d140944e073
Jorge <tetris.attack@hotmail.com>
2019-05-28 04:03:15 -0300

    Thank you LBP, very cool!

---
+++ LBP/lbp-execute.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# vars
+# mudar aqui
+python2=asd  # se 'true', usa python2 em vez de python3
+custompath=sdf  # se 'true', usa o path em $path
+path="../imgs/~rng"
+fancy="****************************************" # decoracao
+
+# nao mudar aqui
+cmd="python3"
+file=""
+
+
+# funcao que roda alguma coisa
+doit () {
+    echo "\n"$fancy"\n"$cmd $file $args"\n"$fancy"\n"
+    $cmd $file $args
+}
+
+if [ "$python2" = true ]; then
+    cmd="python"
+    echo "using python2"
+fi
+
+if [ "$custompath" = true ]; then
+    args="$args -p $path"
+    echo "using custom path $path"
+fi
+
+rm -r results || true # garante execucao mesmo que a pasta nao exista
+mkdir results &&
+
+# roda lbp-detector.py
+file="lbp-detector.py"
+rm -r results/LBP_predictions || true
+mkdir results/LBP_predictions &&
+rm -r results/LBP_nasalpattern || true
+mkdir results/LBP_nasalpattern &&
+doit
