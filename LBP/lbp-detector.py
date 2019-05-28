c1e1a5104f572672a81335fb7b776de675e2a8eb
Jorge <tetris.attack@hotmail.com>
2019-05-28 11:51:21 -0300

    melhor pré-processamento com base na versão mais recente do Wyverson

--- LBP/lbp-detector.py
+++ LBP/lbp-detector.py
@@ -30,6 +30,7 @@ benchmark_file = open('results/LBP_benchmark.txt', 'w') # guarda quantidade de n
 # contador de quais narinas foram encontradas [L, R]. idealmente o resultado seria [n, n] (n = numero de imgs)
 narinas = [0, 0]
 
+
 def cascade_detect(img, cascade, size=200):
     n = 3
     r = cascade.detectMultiScale(img, minNeighbors=n, minSize=(size, size))
@@ -39,85 +40,71 @@ def cascade_detect(img, cascade, size=200):
     return r
 
 
-# Encontra a boca
-def mouth_detector(img):
-    # ajuste de contraste
-    #im = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #im[:,:,2] = cv2.equalizeHist(im[:,:,2])
-    #im = cv2.cvtColor(im, cv2.COLOR_HSV2BGR)
-
-    # imagem negativa
-    #im = cv2.equalizeHist(img)
-    im = 255-img
-
-    # escala de cinza e borrar
-    #im = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-    aux = cv2.GaussianBlur(im, (0, 0), 2)
-
-    # encontra bordas
-    median = np.median(aux)
-    sigma = 0.2
-    lower = 50#int(max(0, (1.0-sigma)*median))
-    upper = 100#int(min(255, (1.0+sigma)*median))
-    aux = cv2.Canny(aux, lower, upper)
-
-    # fechamento (remove pequenos espaços em branco nas linhas)
-    aux = cv2.morphologyEx(aux, cv2.MORPH_CLOSE, cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 3)))
-
-    # encontra as bordas
-    contours, hierarchy = cv2.findContours(aux, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-    # encontra a maior borda horizontalmente
-    tam = 0
-    for c in contours:
-        maxv = np.max(c[:,0,0])
-        minv = np.min(c[:,0,0])
-        if maxv-minv > tam:
-            tam = maxv-minv
-            maior = c
-
-    if tam == 0:
-        return None
-
-    # função da linha
-    z = np.polyfit(maior[:,0,0], maior[:,0,1], 2)
-
-    # COMENTAR AS 2 LINHAS ABAIXO PARA VISUALIZAÇÃO DAS LINHAS ERRADAS
-    # if z[1] > 0 or z[0] < 0:
-    #    return None
-
-    p = np.poly1d(z)
-    return p
-
 # encontra região de interesse (Region Of Interest)
 def ROI(addr, muzzle, narina_r, narina_l):
-    img0 = cv2.imread(addr, cv2.IMREAD_COLOR) # imagem original
-    gray = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY) # imagem escala de cinza
+    img0 = cv2.imread(addr, cv2.IMREAD_COLOR)  # imagem original
+    img0 = cv2.cvtColor(img0, cv2.COLOR_BGR2HSV)
+    img0[:,:,2] = cv2.equalizeHist(img0[:,:,2])
+    img0 = cv2.cvtColor(img0, cv2.COLOR_HSV2BGR)
+    gray = cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY)  # imagem escala de cinza
+
+    ty, tx = np.shape(gray) #tamanho da imagem
+
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    it = clahe.apply(gray)
 
     # procura focinho
-    m = cascade_detect(gray, muzzle)
+    m = cascade_detect(gray, muzzle, int(min(tx, ty)/3))
     # m = lista dos pontos focinhos encontrados
     # m[i] = (x, y, w, h) do i-ésimo focinho
     # x, y = posição do ponto inicial
     # w, h = largura e altura
+
     if len(m) != 1:
         print('Focinho não encontrado.')
         return None
 
+    gray = cv2.equalizeHist(gray)
+
     m = m[0]
-    escala = m[2]/500
-    #print('m2: ', m[2], 'escala: ', escala)
-    focinho = cv2.resize(gray[m[1]:m[1]+m[3], m[0]:m[0]+m[2]], (500, 500))
-    img = cv2.resize(img0[m[1]:m[1]+m[3], m[0]:m[0]+m[2]], (500, 500))
+    dy = m[3]-m[1]
+    dx = m[2]-m[0]
+    dy = int(dy/20)
+    dx = int(dx/20)
+
+    escala = (m[2]+2*dx) / 500
+
+    if m[1]-dy < 0:
+        y0 = 0
+        yf = m[3]+2*dy
+    elif m[1]+m[3]+dy >= ty:
+        yf = ty-1
+        y0 = yf-m[3]-2*dy
+    else:
+        y0 = m[1]-dy
+        yf = m[1]+m[3]+dy
+
+    if m[0]-dx < 0:
+        x0 = 0
+        xf = m[2]+2*dx
+    elif m[0]+m[2]+dx >= tx:
+        xf = tx-1
+        x0 = xf-m[2]-2*dx
+    else:
+        x0 = m[0]-dx
+        xf = m[0]+m[2]+dx
+
+    focinho = cv2.resize(gray[y0:yf, x0:xf], (500, 500))
+    img = cv2.resize(img0[y0:yf, x0:xf], (500, 500))
 
     # procura narinas nos cantos superiores do focinho
-    right = cascade_detect(focinho[:,250:], narina_r, 90)
-    left = cascade_detect(focinho[:,:250], narina_l, 90)
+    right = cascade_detect(focinho[:, 250:], narina_r, 75)
+    left = cascade_detect(focinho[:, :250], narina_l, 75)
     
     # contar narinas encontradas. se nao forem duas, salvar caso de falha e abortar imagem
     narinas[0] += len(left)
     narinas[1] += len(right)
-    if len(left) == 0 or len(right) == 0:  # quero passar por todos os ifs
+    if len(left) == 0 or len(right) == 0:  # quero executar write e return depois de ambos os ifs
         if len(left) == 0:
             print('Narina esquerda não encontrada.')
         if len(right) == 0:
@@ -128,41 +115,25 @@ def ROI(addr, muzzle, narina_r, narina_l):
     left = left[0]
     right = right[0]
 
-    # procura linha da boca na metade inferior da imagem
-    p = mouth_detector(focinho[250:])#, left[0]:250+right[0]+right[2]])#focinho[250:])
-    if not p:
-        print('Boca não identificada.')
-        return None
-
-    # NÃO UTILIZADO: verifica a inclinação da imagem
-    #pl = p(left[0]+(left[2]/2))
-    #pr = p((250+right[0])+(right[2]/2))
-    #if abs(pl-pr) > 10:
-    #    print('Rotaciona')
-
-    # Desenha a linha roxa na imagem e encontra o ponto mais alto da boca
-    menor = 250
-    for i in range(int(left[0]+(left[2]/2)), int((250+right[0])+(right[2])/2)):
-        yp = int(p(i))
-        try:
-            img[249+yp:251+yp, i] = (255, 0, 255)
-        except:
-            continue
-        if yp < menor:
-            menor = yp
-
     # Coordenadas da região de interesse
-    # dentro do corte do focinho
-    w = (250+right[0])-(left[0]+left[2])
+    # dentro da imagem do focinho
+    w = (250 + right[0]) - (left[0] + left[2]) # distancia entre narinas
     h = w
-    x = left[0]+left[2]
-    y = (250+menor) - h
+    x = left[0] + left[2]
+    y = int(((left[1]+left[3]/2)+(right[1]+right[3]/2))/2)
+
+    # aw = int(w*0.15)
+    # ah = int(h*0.15)
+    # x -= aw
+    # y -= ah
+    # w += 2*aw
+    # h += 2*ah
 
     # na imagem original
-    x0 = int(x*escala)+m[0]
-    y0 = int(y*escala)+m[1]
-    x1 = x0+int(w*escala)
-    y1 = y0+int(h*escala)
+    x0 = int(x * escala) + m[0]
+    y0 = int(y * escala) + m[1]
+    x1 = x0 + int(w * escala)
+    y1 = y0 + int(h * escala)
 
     # salva região de interesse
     cv2.imwrite("results/LBP_nasalpattern/" + addr.split("/")[-1], img0[y0:y1, x0:x1])
@@ -194,6 +165,7 @@ def ROI(addr, muzzle, narina_r, narina_l):
     
     return focinho[y:y+h, x:x+w]
 
+# end defs. comeco main
 init = time.time()
 
 muzzle = cv2.CascadeClassifier('cfg/focinho.xml')
@@ -204,7 +176,7 @@ nar_esq = cv2.CascadeClassifier('cfg/esq_BR.xml')
 # gera um array com todos os paths dos .jpg no diretorio
 list_imgs = []
 for root, dirnames, filenames in os.walk(imgs_rootdir):
-    for filename in fnmatch.filter(filenames, '*.jpg'):
+    for filename in fnmatch.filter(filenames, '*.[Jj][Pp][Gg]'): # case insensitive
         list_imgs.append(os.path.join(root, filename))
 
 file_counter = 0 # contador de progresso usado em prints
