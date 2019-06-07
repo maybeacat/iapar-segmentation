03622cadb7e32dcca8462e5fbfc7114c1cf75442
Jorge <tetris.attack@hotmail.com>
2019-06-07 01:20:03 -0300

    sh lbp-execute.sh -h

--- LBP/lbp-execute.sh
+++ LBP/lbp-execute.sh
@@ -1,38 +1,43 @@
 #!/bin/bash
 
-# vars
-# mudar aqui
-python2=asd  # se 'true', usa python2 em vez de python3
-custompath=sdf  # se 'true', usa o path em $path
-path="../imgs/~rng"
-fancy="****************************************" # decoracao
+THIS=$(basename $0)
 
-# nao mudar aqui
-cmd="python3"
-file=""
+# print help
+usage() {
+    echo "$THIS -p <path>"
+    echo "$THIS -x   usa python2"
+    echo "$THIS -h   mostra isso"
+    exit 0
+}
 
-
-# funcao que roda alguma coisa
-doit () {
+# roda alguma coisa
+doit() {
     echo "\n"$fancy"\n"$cmd $file $args"\n"$fancy"\n"
     $cmd $file $args
 }
 
-if [ "$python2" = true ]; then
-    cmd="python"
-    echo "using python2"
-fi
 
-if [ "$custompath" = true ]; then
-    args="$args -p $path"
-    echo "using custom path $path"
-fi
+# defaults
+cmd="python3"
+file="lbp-detector.py"
+fancy="****************************************" # decoracao
+
+
+# processa argumentos
+while getopts "hp:x" OPT; do
+    case "$OPT" in
+    "h") usage;;
+    "p") args="$args -p $OPTARG"; echo "using custom path $OPTARG";;
+    "x") cmd="python"; echo "using python2";;
+    "?") exit 1;;
+    esac
+done
 
 rm -r results || true # garante execucao mesmo que a pasta nao exista
 mkdir results &&
 
 # roda lbp-detector.py
-file="lbp-detector.py"
+
 mkdir results/LBP_predictions &&
 mkdir results/LBP_nasalpattern &&
 doit
