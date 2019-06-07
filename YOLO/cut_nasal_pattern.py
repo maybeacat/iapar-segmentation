03d059c680877749291c2a65f7a49b16f37abf3c
Jorge <tetris.attack@hotmail.com>
2019-06-07 01:36:52 -0300

    add código de rotação do Wyverson

--- YOLO/cut_nasal_pattern.py
+++ YOLO/cut_nasal_pattern.py
@@ -1,6 +1,7 @@
 import cv2
 import os
 import fnmatch
+import numpy as np
 
 # command line
 import sys
@@ -57,14 +58,39 @@ for linha in narinas_file:
         x_esq = min(x1 + w1, x2 + w2)
         x_dir = max(x1, x2)
 
-        dim = x_dir - x_esq
+        ye = y1+0.4*h1
+        yd = y2+0.4*h2
 
-        y_top = int((y1 + 0.4 * h1 + y2 + 0.4 * h2) / 2)
-        y_bot = y_top + dim
+        if x1 > x2:
+            ye, yd = yd, ye
+
+        # rotação
+        if abs(ye-yd) > 5:
+            v1 = [1, 0]
+            v2 = [x_dir-x_esq, yd-ye]
+            dim = int(np.linalg.norm(v2))
+            v2 = v2/np.linalg.norm(v2)
+            theta = np.arctan2(np.linalg.norm(np.cross(v1, v2)), np.dot(v1,v2))
+            if yd < ye:
+                theta = -theta
+            ct = np.cos(theta)
+            st = np.sin(theta)
+            M = np.array([[ct, st, (1-ct)*x_esq - st*ye],
+                          [-st, ct, st*x_esq + (1-ct)*ye]])
+            sy, sx = np.shape(img)[:2]
+            img = cv2.warpAffine(img, M, dsize=(sx, sy))
+
+            y_top = int(ye)
+            y_bot = y_top+dim
+            x_dir = x_esq+dim
+        else:
+            dim = x_dir - x_esq
+            y_top = int((y1 + 0.4 * h1 + y2 + 0.4 * h2) / 2)
+            y_bot = y_top + dim
 
         if y_top < y_bot and x_esq < x_dir:
             roi = img[y_top : y_bot, x_esq : x_dir]
-            cv2.imwrite("YOLO_nasalpattern/" + linha[0], roi)
+            cv2.imwrite("results/YOLO_nasalpattern/" + linha[0], roi)
             coords_file.write("%s,%d,%d,%d,%d\n" % (linha[0], x_esq, y_top, x_dir, y_bot))
 
 # fecha arquivos
